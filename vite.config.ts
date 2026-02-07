fef7862bd2783607fdf074cbd717ba67471ed6aa
anton-zelenskiy <antonfewwt@gmail.com>
2026-02-07 15:17:27 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,26 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('antd') || id.includes('@ant-design')) {
+              return 'antd'
+            }
+            if (id.includes('react-dom') || id.includes('react/')) {
+              return 'react-vendor'
+            }
+            if (id.includes('react-router')) {
+              return 'router'
+            }
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
   server: {
     host: true,
     port: 3001,
